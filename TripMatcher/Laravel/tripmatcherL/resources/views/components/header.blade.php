787ad62ad70328937a82e62c0ac184e09ae64d97
WouterSwerts <swerts.wouter@gmail.com>
2021-09-29 15:54:15 +0200

    finished!!!!!! Thanks all

--- TripMatcher/Laravel/tripmatcherL/resources/views/components/header.blade.php
+++ TripMatcher/Laravel/tripmatcherL/resources/views/components/header.blade.php
@@ -1,6 +1,6 @@
 <div class="header">
    <div class="navBarDark">
-      <img src="{{ asset('assets/img/LogoDarkTheme.png') }}" alt="logo">
+      <a href="http://localhost:4200/"><img src="{{ asset('assets/img/LogoDarkTheme.png') }}" alt="logo"></a>
 
       <a href="http://localhost:4200/">Logout</a>
    </div>
@@ -9,8 +9,8 @@
    </div>
    <div class="subNavBarOption">
 
-         <a href="{{ route('index.create') }}">Add Trip</a>
-         <a href="{{ route('index.store') }}">Edit / Delete Trip</a>
+         <a class="{{ Request::is('index/create') ? 'activeLink' : '' }}" href="{{ route('index.create') }}">Add Trip</a>
+         <a class="{{ Request::is('index') ? 'activeLink' : '' }}" href="{{ route('index.store') }}">Edit / Delete Trip</a>
 
    </div>
 
